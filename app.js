9a891581745f70eeeef92f4226a53ff704b9bc3d
Misael-Lima <misael865.nic@gmail.com>
2025-02-12 20:41:34 -0300

    atualizar a lista e sortear

--- app.js
+++ app.js
@@ -10,6 +10,33 @@ function adicionarAmigo() {
     }
 
     amigos.push(nomeAmigo);
-    console.log(amigos); // Exibe a lista no console
+    console.log(amigos);
     input.value = ""; // Limpa o campo de entrada
+    atualizarLista();
+}
+
+function atualizarLista() {
+    let lista = document.querySelector("#listaAmigos")
+    lista.innerHTML = ""; 
+
+    for (let amigo of amigos) {
+        lista.innerHTML += `<li>${amigo}</li>`; // Adiciona cada amigo como um <li>
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) { 
+        alert("Não há amigos para sortear!");
+        return;
+    }
+
+    //Gerar um índice aleatório
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    
+    //nome sorteado
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    //exibir sorteado
+    let resultado = document.querySelector("#resultado");
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
 }
