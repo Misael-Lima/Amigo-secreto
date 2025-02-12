d3e9fe709c17c4e0b778b9b7d780e23b64faa975
Misael-Lima <misael865.nic@gmail.com>
2025-02-12 19:57:26 -0300

    adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.querySelector("#amigo");
+    let nomeAmigo = input.value.trim();
+
+    if (nomeAmigo === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+    console.log(amigos); // Exibe a lista no console
+    input.value = ""; // Limpa o campo de entrada
+}
